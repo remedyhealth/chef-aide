317b4d78abe8395fad7a1ff6cdca5062b016b5ee
Michael Burns <michael.burns@rackspace.com>
2015-07-05 00:28:13 -0700

    use delayed notification and simplify generate_database script

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,13 +2,8 @@ package 'aide' do
   action :install
 end
 
-# Always re-create the file, to fire a notification
-file node['aide']['config'] do
-  action :delete
-end
-
 template node['aide']['config'] do
-  notifies :run, 'script[generate_database]'
+  notifies :run, 'script[generate_database]', :delayed
 end
 
 template '/etc/cron.d/aide' do
@@ -20,12 +15,8 @@ service node['aide']['cron_service'] do
   action :nothing
 end
 
-# Run by a notification from the template, so it happens at the end of
-# the chef run, picking up all changes that were made
-script 'generate_database' do
+bash 'generate_database' do
   action :nothing
-  interpreter 'bash'
-  unless node['aide']['testmode'] == 'true'
-    code "#{node['aide']['binary']} #{node['aide']['extra_parameters']} --init"
-  end
+  not_if { node['aide']['testmode'] == 'true' }
+  code "#{node['aide']['binary']} #{node['aide']['extra_parameters']} --init"
 end
